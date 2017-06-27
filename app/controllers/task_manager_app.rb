d08d2a3b133fa589fc4df24d986927072e33b975
Daniel Phillips <danieledward.phillips@gmail.com>
2017-06-26 20:22:25 -0600

    adds update and delete

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -3,6 +3,8 @@ require_relative '../models/task.rb'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
+
 
   get '/' do
     erb :dashboard
@@ -32,6 +34,16 @@ class TaskManagerApp < Sinatra::Base
   get '/tasks/:id/edit' do
     @task = Task.find(params[:id])
     erb :edit
+  end
+
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect '/tasks/#{id}'
+  end
+
+  delete '/tasks/:id' do |id|
+      Task.destroy(id.to_i)
+      redirect '/tasks'
   end
 
 end
